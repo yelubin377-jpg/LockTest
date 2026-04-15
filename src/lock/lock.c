e5783990ba1f08f8c4f00dcf778dcb88baff51e0
Michael _377 <yelubin377@gmail.com>
2026-04-15 11:58:30 +0800

    Update lock.c

--- src/lock/lock.c
+++ src/lock/lock.c
@@ -2,14 +2,22 @@
 
 #include <stdio.h>
 
-void amountInit(lock_t* Account) {
-  perror("This function is not implemented");
+void amountInit(lock_t* Account) 
+{
+  Account->amount = 0;
+  pthread_mutex_init(&Account->mutex, NULL);
 }
 
-void Income(lock_t* Account, int amount) {
-  perror("This function is not implemented");
+void Income(lock_t* Account, int amount) 
+{
+  pthread_mutex_lock(&Account->mutex);
+  Account->amount += amount;
+  pthread_mutex_unlock(&Account->mutex);
 }
 
-void Expend(lock_t* Account, int amount) {
-  perror("This function is not implemented");
+void Expend(lock_t* Account, int amount) 
+{
+  pthread_mutex_lock(&Account->mutex);
+  Account->amount -= amount;
+  pthread_mutex_unlock(&Account->mutex);
 }
